9548bc1781b91694af752a5141edf70bff5fd49c
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-14 23:17:13 +0330

    🟥 NEST__JS__INIT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -10,4 +11,3 @@ export class AppController {
     return this.appService.getHello();
   }
 }
- 
