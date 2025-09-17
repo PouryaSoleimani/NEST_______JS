50f9bd4980426c0157d006a406735a53080ce69b
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-18 00:22:57 +0330

    NEST____JS

--- src/main.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./main.controller";
-
-@Module({
-  controllers: [AppController], // DEFINE THE CONTROLLER HERE
-})
-class AppModule {}
-
-export default AppModule;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,34 @@
 import { Controller, Get, Module } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import { AppController } from "./main.controller";
-import AppModule from "./main.module";
+
+// DATA
+const mockDataArray = [
+  { id: 1, name: "POURYA" },
+  { id: 2, name: "MAMAD" },
+];
+
+// CONTROLLER
+@Controller()
+class AppController {
+  @Get("/")
+  getRootRoute() {
+    return {
+      message: "THE FIRST NEST__JS RESPONSE",
+      data: mockDataArray,
+    };
+  }
+}
+
+// MODULE
+@Module({
+  controllers: [AppController], // DEFINE THE CONTROLLER HERE
+})
+class AppModule {}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 
+// BOOTSTRAP
 bootstrap();
