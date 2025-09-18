72f98837e987f71274b231db399e4caba94d5f8b
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-19 00:52:46 +0330

    NEST___JS___ROUTING

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./main.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000, () => {
-    console.log("SERVER IS LISTENING AT PORT 3000");
+    console.log("🖥️    SERVER IS LISTENING AT PORT 3000 ");
   });
 }
 
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 
-@Controller()
+@Controller("/api")
 export class UserController {
   @Get("/users")
   getAllUser() {
     return "HELLO FROM USERS";
   }
+
   @Get("/users/:id")
   getSingleUser() {
     return "SINGLE USER ROUTE";
+  }
+
+  @Post("/users/:id")
+  PostSingleUser() {
+    const data = { id: 1, name: "POURYA", job: "DEVELOPER" };
+    return {
+      ok: true,
+      message: "SINGLE USER POST ROUTE",
+      data: data,
+      id: "1",
+    };
   }
 }
