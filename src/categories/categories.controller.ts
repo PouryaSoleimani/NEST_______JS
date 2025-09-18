230a632eaea61f036ffedd524c9f476c908a044d
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-19 01:39:14 +0330

    NEST___JS___ROUTING

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get, Post } from "@nestjs/common";
 
 @Controller("/api")
 export class CategoriesController {
   @Get("/categories")
   getAllCategories() {
     return "ALL CATEGORIES ROUTE";
+  }
+  @Get("/categories/:id")
+  getSingleCategory() {
+    return "SINGLE CATEGORY ROUTE";
+  }
+  @Post("categories/:id")
+  PostSingleCategory() {
+    return "SINGLE CATEGORY ++POST++ ROUTE";
+  }
+  @Delete("categories/:id")
+  DeleteSingleCategory() {
+    return "SINGLE CATEGORY ++DELETE++ ROUTE";
   }
 }
