f9f5e00b2f14b8d1473588e28286fd306379f8fa
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-18 22:40:15 +0330

    NEST___JS___MODULE

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
-import { Controller, Get, Module } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import { AppController } from "./main.controller";
 import { AppModule } from "./main.module";
-
-const mockDataArray = [
-  { id: 1, name: "POURYA" },
-  { id: 2, name: "MAMAD" },
-];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 
-// BOOTSTRAP
 bootstrap();
