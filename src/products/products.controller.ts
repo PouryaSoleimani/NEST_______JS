074d8d98c3547a421c6f469016dfd13e71f8f8c8
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-19 22:38:11 +0330

    NEST___JS___PIPES__VALIDATIONS___1

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,19 +1,31 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { _Products } from "./../../__data__/db.js";
 import { createSingleProductDTO } from "./product.dto.js";
 
 @Controller("/api/products")
 export class ProductsController {
   @Get("/")
-  getAllProduts() {
+  GET__ALL__PRODUCTS() {
     return {
       ok: true,
       message: "ALL PRODUCTS ROUTE ==GET==",
       allProducts: _Products,
     };
   }
+
+  @Get("/:id")
+  GET__SINGLE__PRODUCT(@Param() single_product_id) {
+    const singleProduct = _Products.find((item: any) => item.id === Number(single_product_id.id));
+    return {
+      ok: singleProduct ? true : false,
+      message: singleProduct ? "PRODUCT__FOUND ==200==" : "PRODUCT__NOT__FOUND ==400==",
+      product: singleProduct ? singleProduct : undefined,
+      param: single_product_id,
+    };
+  }
+
   @Post("/:id")
-  PostSingleProduct(@Body() postReqBody: createSingleProductDTO) {
+  POST__SINGLE__PRODUCT(@Body() postReqBody: createSingleProductDTO) {
     _Products.push(postReqBody);
     return {
       ok: true,
