970dc610e839d1079f3d0bf0c75423672add48e0
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-19 13:44:02 +0330

    NEST___JS___PARAM__BODY_QUERY

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
 
 @Controller("/api/posts")
 export class PostsController {
@@ -6,12 +6,28 @@ export class PostsController {
   GET_ALL_POSTS() {
     return "ALL POSTS ==GET==";
   }
+
   @Get("/:id")
-  GET_SINGLE_POST() {
-    return "SINGLE POST ==GET==";
+  GET_SINGLE_POST(@Param("id") single_post_get_param) {
+    return {
+      ok: true,
+      message: "GET_SINGLE_POST_ROUTE",
+      params: [single_post_get_param],
+    };
   }
+
   @Post("/:id")
-  POST_SINGLE_POST() {
-    return "SINGLE POST ==POST==";
+  POST_SINGLE_POST(
+    @Body() single_post_datas: any,
+    @Query() single_post_query: { [name: string]: string },
+    @Param() single_post_param: number,
+  ) {
+    return {
+      ok: true,
+      message: "POST SINGLE ROUTE ==POST==",
+      data: single_post_datas,
+      query: single_post_query,
+      param: single_post_param,
+    };
   }
 }
