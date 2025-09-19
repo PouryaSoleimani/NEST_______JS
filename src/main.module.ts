9d016a6b1b1f481d35178c6b7d2c7764b0fa5f40
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-19 22:19:16 +0330

    NEST___JS___PIPES__VALIDATIONS___1

--- src/main.module.ts
+++ src/main.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./main.controller";
 import { UserController } from "./users/user.controller";
 import { PostsController } from "./posts/posts.controller";
 import { ProductsModule } from "./products/products.module";
+import { ProductsController } from "./products/products.controller";
 
 @Module({
-  controllers: [AppController, UserController, PostsController, ProductsModule],
+  controllers: [AppController, UserController, PostsController, ProductsController],
   imports: [],
 })
 export class MainModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { MainModule } from "./main.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(MainModule);
+  const app = await NestFactory.create(MainModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000, () => {
     console.log("🖥️    SERVER IS LISTENING AT PORT 3000 ");
