d3e75636affa0d8b3a1ee08fd22a19f92ffbd0b9
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-22 23:48:34 +0330

    🟦 NEST__JS__REPOSITORY

--- products/products.pipe.ts
+++ products/products.pipe.ts
@@ -15,13 +15,10 @@ export class UpdateSingleProductDTO {
   @IsOptional()
   @IsNumber()
   id: number;
-
   @IsString()
   title: string;
-
   @IsNumber()
   price: number;
-
   @IsBoolean()
   isAvailable: boolean;
 }
