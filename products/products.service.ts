456e62df51912c61ce31c055e463e44e2c791e02
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-24 21:53:42 +0330

    🟨 NEST___JS____EXCEPTION____FILTERS

--- products/products.service.ts
+++ products/products.service.ts
@@ -7,6 +7,7 @@ import { ProductsRepository } from "./products.repository";
 
 @Injectable()
 export class ProductsService {
+  
   productsRepo: ProductsRepository;
   constructor() {
     this.productsRepo = new ProductsRepository();
