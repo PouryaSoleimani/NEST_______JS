a7fb2dde9dfdfc2f78a17dffb251e031a2b2e5a3
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-26 00:54:24 +0330

    🟪 NEST___JS____DEP{ENDENCY____INJECTION

--- src/main.module.ts
+++ src/main.module.ts
@@ -3,9 +3,9 @@ import { AppController } from "./main.controller";
 import { UsersController } from "./users/user.controller";
 import { PostsController } from "./posts/posts.controller";
 import { CategoriesController } from "./categories/categories.controller";
-import { ProductsController } from "products/products.controller";
-import { ProductsService } from "products/products.service";
-import { ProductsRepository } from "products/products.repository";
+import { ProductsController } from "src/categories/products/products.controller";
+import { ProductsService } from "src/categories/products/products.service";
+import { ProductsRepository } from "src/categories/products/products.repository";
 
 @Module({
   controllers: [
