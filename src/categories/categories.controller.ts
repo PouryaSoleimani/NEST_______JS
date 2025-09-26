b381aeff7498d3a93f3140f7bee2f26856439c17
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-26 13:11:12 +0330

    🟥 NEST__JS__MODULES___DI

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -2,45 +2,49 @@ import { BadRequestException, Body, Controller, Get, Param, Post } from "@nestjs
 import { ALL__DATAS } from "../../__data__/db.js";
 import { CategoriesPostDTO } from "./categories.pipe.js";
 import { ArticlesService } from "src/articles/articles.service.js";
+import { CategoriesService } from "./categories.service.js";
 
 @Controller("/api/categories")
 export class CategoriesController {
-  constructor(public ArticlesService: ArticlesService) {}
+  constructor(
+    public CategoriesService: CategoriesService,
+    public ArticlesService: ArticlesService,
+  ) {}
 
   @Get("/")
-  GET__ALL__CATEGORIES() {
-    return {
-      ok: true,
-      message: "GET ALL CATEGORIES ==GET== ROUTE",
-      data: ALL__DATAS._Categories,
-    };
+  GET___ALL___CATEGORIES() {
+    const result = this.CategoriesService.GET__ALL__CATEGORIES();
+    if (!result) {
+      throw new BadRequestException("BAD REQUEST");
+    } else {
+      return result;
+    }
   }
 
   @Get("/:id")
-  GET__SINGLE__CATEGORY(@Param("id") single_category_id) {
-    const _Category = ALL__DATAS._Categories.find(
-      (item: CategoriesPostDTO) => item.id == single_category_id,
-    );
-    return {
-      ok: true,
-      message: _Category ? "GET ALL CATEGORIES ==GET== ROUTE" : "CATEGORY NOT FOUND !",
-      data: _Category ? _Category : null,
-    };
+  GET___SINGLE___CATEGORY(@Param("id") single_category_id) {
+    const result = this.CategoriesService.GET__SINGLE__CATEGORY(+single_category_id);
+    if (!result) {
+      throw new BadRequestException("BAD REQUEST");
+    } else {
+      return result;
+    }
   }
 
   @Post("/create")
   CREATE_SINGLE_CATEGORY(@Body() single_category_req_body: CategoriesPostDTO) {
-    ALL__DATAS._Categories.push(single_category_req_body);
-    return {
-      ok: true,
-      message: "201 - CATEGORY CREATED SUCCESSFULLY",
-      body: single_category_req_body,
-      data: ALL__DATAS._Categories,
-    };
+    const result = this.CategoriesService.CREATE_SINGLE_CATEGORY(
+      single_category_req_body,
+    );
+    if (!result) {
+      throw new BadRequestException("BAD REQUEST");
+    } else {
+      return result;
+    }
   }
 
   @Get("/articles/getAll")
-  GET__ALL__ARTICELS__IN__CATEGORIES() {
+  GET___ALL___ARTICELS___IN___CATEGORIES() {
     const result = this.ArticlesService.GET_ALL_ARTICLES();
     if (!result) {
       throw new BadRequestException("BAD REQUEST !!!");
