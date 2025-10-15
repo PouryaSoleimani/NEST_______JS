f453db55dda61494e30b9767bf74a455edabc50e
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-10-16 00:22:02 +0330

    EMPLOYEES___CRUD

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 
 @Controller('/employees')
@@ -6,6 +6,10 @@ export class EmployeesController {
   constructor(private readonly service: EmployeesService) { }
   @Get("/")
   getAll() {
-    return 'ALL EMPLOYEES'
+    return this.service.getAll()
+  }
+  @Post('/create')
+  create(@Body() body: any) {
+    return this.service.create(body)
   }
 }
--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,10 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { timeStamp } from 'console';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class EmployeesService {
   constructor(private readonly prisma: PrismaService) { }
-  getAll() {
+
+  async getAll() {
+    const result = await this.prisma.employee.findMany()
+    if (!result) {
+      throw new NotFoundException('404 | NO EMPLOYEES FOUND')
+    } else {
+      return {
+        ok: true,
+        message: 'EMPLOYEES LIST',
+        data: result
+      }
+    }
+  }
+
+  async create(body: any) {
+    const newEmployee = await this.prisma.employee.create({
+      data: {
+        fullname: body.fullname,
+        job: body.job,
+      }
+    })
+    if (!newEmployee) {
+      throw new BadRequestException('400 | BAD REQUEST')
+    } else {
+      return {
+        ok: true,
+        message: '201 | NEW EMPLOYEE CREATED SUCCESSFULLY',
+        newEmployee: newEmployee
+      }
+    }
   }
 }
