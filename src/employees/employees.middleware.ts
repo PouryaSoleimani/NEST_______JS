c43af6699ad952486903d1963f2aad47b158bf24
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-10-16 11:30:10 +0330

    🟪 NEST --> MIDDLEWARES

--- src/employees/employees.middleware.ts
+++ src/employees/employees.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express"
 
 export function EmployeesMiddleWare(req: Request, res: Response, next: NextFunction) {
-  console.log("🖥️ WELCOME TO EMPLOYEES ROUTE")
+  console.log("🖥️  WELCOME TO EMPLOYEES ROUTE",)
   console.log('BASE URL ==> ', req.baseUrl)
   console.log('PATH ==> ', req.path)
   next()
