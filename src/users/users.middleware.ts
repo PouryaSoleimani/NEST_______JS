23fa2d0daba53ad81c800593bf1432930116a7a6
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-10-16 12:13:43 +0330

    🟪 NEST --> MIDDLEWARES

--- src/users/users.middleware.ts
+++ src/users/users.middleware.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from "express";
 export class UsersMiddleWare implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log('👥 USERS ROUTE')
-    console.log("👥 BASE URL =>", req.baseUrl)
+    console.log("👥 URL =>", req.url)
     console.log("👥 PATH =>", req.path)
     next()
   }
