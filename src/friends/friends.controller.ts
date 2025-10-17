2c99e749133a0752dac7e9e12c6d40e5136ec8c5
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-10-17 18:59:21 +0330

    FRIENDS___CRUD

--- src/friends/friends.controller.ts
+++ src/friends/friends.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseInterceptors } from "@nestjs/common";
 import { FriendsService } from "./friends.service";
 import { CreateSingleFriendDTO } from "./friends.pipe";
+import { FriendsCreateInterceptor, FriendsGetAllInterceptor } from "./friends.interceptor";
 
 @Controller("/friends")
 export class FriendsController {
   constructor(private readonly service: FriendsService) {}
 
+  @UseInterceptors(FriendsGetAllInterceptor) // ADDING INTERCEPTOR HERE
   @Get("")
   getAll() {
     return this.service.getAll();
   }
 
+  @UseInterceptors(FriendsCreateInterceptor)
   @Post("/create")
   create(@Body() body: CreateSingleFriendDTO) {
     return this.service.create(body);
--- src/friends/friends.interceptor.ts
+++ src/friends/friends.interceptor.ts
@@ -1,15 +1,43 @@
-/*
-https://docs.nestjs.com/interceptors#interceptors
-*/
-
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
-import { Observable } from "rxjs";
+import { Observable, map } from "rxjs";
 import { tap } from "rxjs/operators";
 
 @Injectable()
-export class FriendsInterceptor implements NestInterceptor {
+export class FriendsGetAllInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log("Before...");
-    return next.handle().pipe(tap(() => console.log(`After...`)));
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          ok: true,
+          message: "ALL FRIENDS LIST FROM INTERCEPTOR",
+          data: data.data.map((item) => {
+            return {
+              id: item.id,
+              name: item.name,
+              age: item.age,
+              gender: item.gender,
+            };
+          }),
+        };
+      }),
+    );
+  }
+}
+
+export class FriendsCreateInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          ok: true,
+          message: "201 | USER CREATED | INTERCEPTOR",
+          data: {
+            id: data.data.id,
+            fullname: data.data.name,
+          },
+        };
+      }),
+    );
   }
 }
