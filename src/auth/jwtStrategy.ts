ab99a053540ada34cba889ac58ba399815919025
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-11-08 00:43:00 +0330

    JWT

--- src/auth/jwtStrategy.ts
+++ src/auth/jwtStrategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: "secret____key",
+      secretOrKey: "POURYA__SECRET",
     });
   }
 
