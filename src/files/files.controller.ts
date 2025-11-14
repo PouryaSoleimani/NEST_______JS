03e8abc9e016a8cc0b35948d29afbf45068c4948
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-11-14 13:55:58 +0330

    🟥 NEST__JS ===> FILE___UPLOAD

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import path from "node:path";
 
 @Controller("/files")
 export class FilesController {
@@ -9,7 +11,18 @@ export class FilesController {
   }
 
   @Post("/upload")
-  @UseInterceptors(FileInterceptor("image")) // 1
+  @UseInterceptors(
+    FileInterceptor("image", {
+      storage: diskStorage({
+        destination: "./uploads",
+        filename:(req , file,cb) => {
+          const fileName = path.parse(file.originalname).name;
+          const extension = path.parse(file.originalname).ext;
+          cb(null, `${fileName}-${Date.now}${extension}`);
+        }
+      }),
+    }),
+  ) // 1
   uploadFile(@Body() body: any, @UploadedFile() file: Express.Multer.File) {
     // 2
     return {
