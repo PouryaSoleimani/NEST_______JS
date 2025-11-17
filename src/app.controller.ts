2c9c2bb45721d39ab5b54a167a25ef118b1a8332
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-11-17 09:44:52 +0330

    🟧 TEMPLATE___ENGINES

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,10 @@
 // ? MAIN CONTROLLER
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Render } from "@nestjs/common";
 @Controller()
 export class AppController {
+  @Get()
+  @Render("root")
+  async getRoot() {
+    return { text: "NEST JS TRAINING " };
+  }
 }
