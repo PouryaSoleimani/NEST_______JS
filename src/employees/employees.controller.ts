af629ad7283da8ac0abcad50ad38f9dbd27e6517
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-07 08:07:42 +0330

    PRETTIER

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,35 +1,24 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-  Post,
-  UseInterceptors,
-} from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Post, UseInterceptors } from "@nestjs/common";
 import { EmployeesService } from "./employees.service";
-import {
-  EmployeesGetAllInterceptor,
-  EmployeesGetSingleInterceptor,
-} from "./employees.interceptor";
+import { EmployeesGetAllInterceptor, EmployeesGetSingleInterceptor } from "./employees.interceptor";
 
 @Controller("/employees")
 export class EmployeesController {
-  constructor(private readonly service: EmployeesService) {}
-  @UseInterceptors(EmployeesGetAllInterceptor)
-  @Get("/")
-  getAll() {
-    return this.service.getAll();
-  }
+   constructor(private readonly service: EmployeesService) {}
+   @UseInterceptors(EmployeesGetAllInterceptor)
+   @Get("/")
+   getAll() {
+      return this.service.getAll();
+   }
 
-  @UseInterceptors(EmployeesGetSingleInterceptor)
-  @Get("/:id")
-  getSingle(@Param("id", ParseIntPipe) id: number) {
-    return this.service.getSingle(+id);
-  }
+   @UseInterceptors(EmployeesGetSingleInterceptor)
+   @Get("/:id")
+   getSingle(@Param("id", ParseIntPipe) id: number) {
+      return this.service.getSingle(+id);
+   }
 
-  @Post("/create")
-  create(@Body() body: any) {
-    return this.service.create(body);
-  }
+   @Post("/create")
+   create(@Body() body: any) {
+      return this.service.create(body);
+   }
 }
