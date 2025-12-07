06b57c2e2fa572837d374f000a86859ef116b649
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-07 08:07:58 +0330

    PRETTIER

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -3,52 +3,52 @@ import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class EmployeesService {
-  constructor(private readonly prisma: PrismaService) { }
+   constructor(private readonly prisma: PrismaService) {}
 
-  async getAll() {
-    const result = await this.prisma.employee.findMany();
-    if (!result) {
-      throw new NotFoundException("404 | NO EMPLOYEES FOUND");
-    } else {
-      return {
-        ok: true,
-        message: "EMPLOYEES LIST",
-        data: result,
-      };
-    }
-  }
+   async getAll() {
+      const result = await this.prisma.employee.findMany();
+      if (!result) {
+         throw new NotFoundException("404 | NO EMPLOYEES FOUND");
+      } else {
+         return {
+            ok: true,
+            message: "EMPLOYEES LIST",
+            data: result,
+         };
+      }
+   }
 
-  async getSingle(id: number) {
-    const singleEmployee = await this.prisma.employee.findUnique({
-      where: { id: id },
-    });
+   async getSingle(id: number) {
+      const singleEmployee = await this.prisma.employee.findUnique({
+         where: { id: id },
+      });
 
-    if (!singleEmployee) {
-      throw new NotFoundException("404 | EMPLOYEE NOT FOUND");
-    } else {
-      return {
-        ok: true,
-        message: "200 | SINGLE EMPLOYEE ROUTE",
-        data: singleEmployee,
-      };
-    }
-  }
+      if (!singleEmployee) {
+         throw new NotFoundException("404 | EMPLOYEE NOT FOUND");
+      } else {
+         return {
+            ok: true,
+            message: "200 | SINGLE EMPLOYEE ROUTE",
+            data: singleEmployee,
+         };
+      }
+   }
 
-  async create(body: any) {
-    const newEmployee = await this.prisma.employee.create({
-      data: {
-        fullname: body.fullname,
-        job: body.job,
-      },
-    });
-    if (!newEmployee) {
-      throw new BadRequestException("400 | BAD REQUEST");
-    } else {
-      return {
-        ok: true,
-        message: "201 | NEW EMPLOYEE CREATED SUCCESSFULLY",
-        newEmployee: newEmployee,
-      };
-    }
-  }
+   async create(body: any) {
+      const newEmployee = await this.prisma.employee.create({
+         data: {
+            fullname: body.fullname,
+            job: body.job,
+         },
+      });
+      if (!newEmployee) {
+         throw new BadRequestException("400 | BAD REQUEST");
+      } else {
+         return {
+            ok: true,
+            message: "201 | NEW EMPLOYEE CREATED SUCCESSFULLY",
+            newEmployee: newEmployee,
+         };
+      }
+   }
 }
