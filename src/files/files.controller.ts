24d62387dc8f113b6f13edec6fe584541a7ba5e9
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-08 18:04:31 +0330

    adsdad

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,78 +1,66 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  Render,
-  Res,
-  UploadedFile,
-  UseInterceptors,
-  Version,
-} from "@nestjs/common";
+import { BadRequestException, Body, Controller, Get, Param, Post, Render, Res, UploadedFile, UseInterceptors, Version } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { diskStorage } from "multer";
 import path from "node:path";
 
 @Controller("/files")
 export class FilesController {
-  @Get("/get-all")
-  @Version("1")
-  getAll() {
-    return "GET ALL FILES --VERSION 1";
-  }
+   @Get("/get-all")
+   @Version("1")
+   getAll() {
+      return "GET ALL FILES --VERSION 1";
+   }
 
-  @Get("/get-all")
-  @Version("2")
-  getAll2() {
-    return "V2";
-  }
+   @Get("/get-all")
+   @Version("2")
+   getAll2() {
+      return "V2";
+   }
 
-  // UPLOAD FILE ___________________________________________________________________________________________________________
-  @Post("/upload")
-  @UseInterceptors(
-    FileInterceptor("image", {
-      storage: diskStorage({
-        destination: "./uploads",
-        filename: (req, file, cb) => {
-          const fileName = path.parse(file.originalname).name;
-          const extension = path.parse(file.originalname).ext; //image.jpg
-          cb(null, `${fileName}-${Date.now()}${extension}`);
-          console.info(req);
-        },
-      }),
-      // VALIDATIONS
-      fileFilter: (req, file, cb) => {
-        const extension = path.parse(file.originalname).ext;
-        const size = file.size;
-        if (extension != ".jpg" || size >= 10000000) {
-          return cb(new BadRequestException("400 | BAD REQUEST"), false);
-        }
-        console.info(req);
-        return cb(null, true);
-      },
-      limits: { fileSize: 10000000 },
-    }),
-  ) // 1
-  uploadFile(@Body() body: any, @UploadedFile() file: Express.Multer.File) {
-    // 2
-    return {
-      body: body,
-      file: file, // 3
-    };
-  }
+   // UPLOAD FILE ___________________________________________________________________________________________________________
+   @Post("/upload")
+   @UseInterceptors(
+      FileInterceptor("image", {
+         storage: diskStorage({
+            destination: "./uploads",
+            filename: (req, file, cb) => {
+               const fileName = path.parse(file.originalname).name;
+               const extension = path.parse(file.originalname).ext; //image.jpg
+               cb(null, `${fileName}-${Date.now()}${extension}`);
+               console.info(req);
+            },
+         }),
+         // VALIDATIONS
+         fileFilter: (req, file, cb) => {
+            const extension = path.parse(file.originalname).ext;
+            const size = file.size;
+            if (extension != ".jpg" || size >= 10000000) {
+               return cb(new BadRequestException("400 | BAD REQUEST"), false);
+            }
+            console.info(req);
+            return cb(null, true);
+         },
+         limits: { fileSize: 10000000 },
+      })
+   ) // 1
+   uploadFile(@Body() body: any, @UploadedFile() file: Express.Multer.File) {
+      // 2
+      return {
+         body: body,
+         file: file, // 3
+      };
+   }
 
-  // GET IMAGE _________________________________________________________________________________________________________
-  @Get("/image/:image")
-  showImage(@Param("image") img: string, @Res() res: any) {
-    return res.sendFile(img, { root: "./uploads" });
-  }
+   // GET IMAGE _________________________________________________________________________________________________________
+   @Get("/image/:image")
+   showImage(@Param("image") img: string, @Res() res: any) {
+      return res.sendFile(img, { root: "./uploads" });
+   }
 
-  // RENDER HTML VIEW PAGE _________________________________________________________________________________________________________
-  @Get("get-html")
-  @Render("index")
-  renderHtml() {
-    return { name: "pourya", age: 32 };
-  }
+   // RENDER HTML VIEW PAGE _________________________________________________________________________________________________________
+   @Get("get-html")
+   @Render("index")
+   renderHtml() {
+      return { name: "pourya", age: 32 };
+   }
 }
