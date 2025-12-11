d1c8a0607b53d78e839e6e2973aec83110d150ba
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-11 09:44:50 +0330

    REFACTOR:

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -3,8 +3,8 @@ import { EmployeesController } from "./employees.controller";
 import { Module } from "@nestjs/common";
 
 @Module({
-  imports: [],
-  controllers: [EmployeesController],
-  providers: [EmployeesService],
+   imports: [],
+   controllers: [EmployeesController],
+   providers: [EmployeesService],
 })
 export class EmployeesModule {}
