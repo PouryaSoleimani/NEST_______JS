b79408065d9ae4f576ffd88db8529f73ed6b551b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-14 11:43:54 +0330

    🟧 TEMPLATE___ENGINES

--- src/main.ts
+++ src/main.ts
@@ -8,49 +8,45 @@ import helmet from "helmet";
 import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
 
-  // VIEW RENDERING
-  // npm i hbs - (handlebars)
-  app.setBaseViewsDir(path.join(__dirname, "..", "views"));
-  app.useStaticAssets(path.join(__dirname, "..", "public"));
-  app.setViewEngine("hbs");
+   // VIEW RENDERING
+   // npm i hbs - (handlebars)
+   app.setBaseViewsDir(path.join(__dirname, "..", "views"));
+   app.useStaticAssets(path.join(__dirname, "..", "public"));
+   app.setViewEngine("hbs");
 
-  // GLOBAL PIPES
-  app.useGlobalPipes(new ValidationPipe());
+   // GLOBAL PIPES
+   app.useGlobalPipes(new ValidationPipe());
 
-  // GLOBAL PREFIX
-  app.setGlobalPrefix("/api");
+   // GLOBAL PREFIX
+   app.setGlobalPrefix("/api");
 
-  // CORS
-  app.enableCors({
-    origin: ["https://webprog.io"],
-  });
+   // CORS
+   app.enableCors({
+      origin: ["https://webprog.io"],
+   });
 
-  app.use(helmet()); // HELMET IMPROVES SECURITY IN OUR PROJECTS
+   app.use(helmet()); // HELMET IMPROVES SECURITY IN OUR PROJECTS
 
-  // FAVICON
-  app.use(serveFavicon(path.join(__dirname, "..", "src", "public", "favicon.ico")));
+   // FAVICON
+   app.use(serveFavicon(path.join(__dirname, "..", "src", "public", "favicon.ico")));
 
-  // SWAGGER
-  const config = new DocumentBuilder()
-    .setTitle("NEST JS TRAINING")
-    .setDescription("THIS IS MY SWAGGER FOR TRAINING NEST JS")
-    .setVersion("1.0")
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("/api/docs", app, documentFactory);
+   // SWAGGER
+   const config = new DocumentBuilder().setTitle("NEST JS TRAINING").setDescription("THIS IS MY SWAGGER FOR TRAINING NEST JS").setVersion("1.0").build();
+   const documentFactory = () => SwaggerModule.createDocument(app, config);
+   SwaggerModule.setup("/api/docs", app, documentFactory);
 
-  // VERSIONING
-  app.enableVersioning({
-    defaultVersion: "1",
-    type: VersioningType.URI,
-  });
+   // VERSIONING
+   app.enableVersioning({
+      defaultVersion: "1",
+      type: VersioningType.URI,
+   });
 
-  // PORT
-  const port = process.env.PORT || 8000;
-  await app.listen(port, () => {
-    console.log(`🖥️  SERVER IS LISTENING AT PORT ${ port }`);
-  });
+   // PORT
+   const port = process.env.PORT || 8000;
+   await app.listen(port, () => {
+      console.log(`🖥️  SERVER IS LISTENING AT PORT ${port}`);
+   });
 }
 bootstrap();
