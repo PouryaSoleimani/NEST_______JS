cd463c2d946d51346d1dba0cc77e15378446308b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-15 23:40:28 +0330

    REFACTOR

--- src/employees/employees.middleware.ts
+++ src/employees/employees.middleware.ts
@@ -2,8 +2,8 @@ import { NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 
 export function EmployeesMiddleWare(req: Request, res: Response, next: NextFunction) {
-  console.log("👤  WELCOME TO EMPLOYEES ROUTE");
-  console.log("BASE URL ==> ", req.baseUrl);
-  console.log("PATH ==> ", req.path);
-  next();
+   console.log("👤  WELCOME TO EMPLOYEES ROUTE");
+   console.log("BASE URL ==> ", req.baseUrl);
+   console.log("PATH ==> ", req.path);
+   next();
 }
